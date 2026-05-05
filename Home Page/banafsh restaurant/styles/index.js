1e7fec3a3787c0d2af5ccfb919fe3c1ff55f0c85
FaridGhorashi1998 <246288434+sfaridghorashi@users.noreply.github.com>
2026-05-05 23:27:16 +0330

    Add file via upload

---
+++ Home Page/banafsh restaurant/styles/index.js
@@ -0,0 +1,12 @@
+//popup
+/*
+const popup = document.querySelector("#popup")
+
+setTimeout( () => {popup.classList.add("popup")},3000);
+
+function closepopup () {
+    popup.classList.remove("popup");
+}
+
+popup.addEventListener("click",closepopup);*/
+
